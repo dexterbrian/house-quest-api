6424aba650a0efe44ef1a0f01aa84f200c0f5f05
= <brianwaweru627@gmail.com>
2023-07-31 00:20:35 +0300

    Enabled updating & deleting owners

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -183,4 +183,85 @@ class ApplicationController < Sinatra::Base
     )
   end
 
+  patch "/owners/:id" do
+    owner = Owner.find(params[:id])
+    owner.update(
+      name: params[:name],
+      email: params[:email],
+      phone: params[:phone]
+    )
+    owner.to_json(
+      only: [
+        :id,
+        :name,
+        :email,
+        :phone,
+        :created_at
+      ],
+      include: {
+        properties: {
+          only: [
+            :id,
+            :owner_id,
+            :house_type,
+            :rent,
+            :rent_due_date,
+            :city,
+            :location,
+            :building,
+            :floor,
+            :coordinates,
+            :other_info,
+            :created_at
+          ]
+        }
+      }
+    )
+  end
+
+  delete "/owners/:id" do
+    begin
+      owner = Owner.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      {
+        error: true,
+        message: "Record not found"
+      }.to_json
+    else
+      owner.destroy
+
+      # Delete properties owned by this owner
+      properties = Property.where(owner_id: params[:id])
+      properties.destroy_all
+      
+      owner.to_json(
+        only: [
+          :id,
+          :name,
+          :email,
+          :phone,
+          :created_at
+        ],
+        include: {
+          properties: {
+            only: [
+              :id,
+              :owner_id,
+              :house_type,
+              :rent,
+              :rent_due_date,
+              :city,
+              :location,
+              :building,
+              :floor,
+              :coordinates,
+              :other_info,
+              :created_at
+            ]
+          }
+        }
+      )
+    end
+  end
+
 end
